6348fa5ad0ff10dd05561ffbd153fc988e7b06d0
cmyyx <140233372+cmyyx@users.noreply.github.com>
2025-12-22 17:40:58 +0800

    意外删除

--- convert.js
+++ convert.js
@@ -686,6 +686,7 @@ function main(config) {
             "name": "GLOBAL",
             "icon": "https://cdn.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Global.png",
             "include-all": true,
+            "type": "select",
             "proxies": globalProxies
         }
     );
